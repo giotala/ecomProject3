245a56b0c1fa928c0c504af9980d506e9c1711cf
Gio Talabong <gio.talabong@gmail.com>
2020-10-19 07:27:34 -0700

    fixed some keys

--- constants/stripe.js
+++ constants/stripe.js
@@ -1,8 +1,8 @@
 const configureStripe = require('stripe');
  
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? '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'
-    : 'sk_test_51Hbf1mExXOpK3cOruAO41I1L7faBEIUa8D54hYLqqzLMkNnHYrRgTMZdvEI2wSDXXKQ7DS3qlMGUSn68LmqVlenO002xZrHrLk';
+    ? process.env.SECRET_KEY_LIVE
+    : process.env.SECRET_KEY;
  
 const stripe = configureStripe(STRIPE_SECRET_KEY);
  
